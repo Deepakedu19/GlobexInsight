7e046c59e940088c5ae44b95ade4c2417f98c287
Nanda Kishore <kishoretanuku1222@gmail.com>
2025-06-20 19:37:45 +0530

    this is a commit from kishore

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Globex Insight</h1>
+        <h1>the future of data</h1>
       </header>
     </div>
   );
